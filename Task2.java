c4e9cfc3173a07547928ff6bcd7fcae2ed6b3d10
Alexander Abrosimov <abrosimov.work@mail.ru>
2020-01-16 20:15:45 +0300

    Еще одно исправление дз

--- Task2.java
+++ Task2.java
@@ -2,23 +2,17 @@ package homework1.task1;
 
 public class Task2 {
     public static void main(String[] args) {
-        int a = 1;
+        int a = 6;
         int b = 3;
         boolean c = method2(a, b);
         System.out.println("Результат=" + c);
     }
 
     public static boolean method2(int a, int b) {
-        boolean c;
-        if ((a + b) >= 10 && (a + b) <= 20)
-            c = true;
-        else
-            c = false;
+        boolean c = (a + b) >= 10 && (a + b) <= 20;
         return c;
     }
 }
-
-
 
 
 
